8b5e44b6578641d8783e6e79515b97011e3c16f1
Julian Bohnenkämper <julianbohnenkaemper@gmail.com>
2023-06-24 19:59:53 +0200

    Added batch files back

---
+++ example/config.bat
@@ -0,0 +1,16 @@
+@rem ----- Builtch Configuration -----
+ 
+@rem ------------- Files -------------
+set source_file=example.c
+set output_file=example.exe
+ 
+@rem ----------- Arguments -----------
+set common_args=-Wall
+set debug_args=-D _DEBUG
+set release_args=-D NDEBUG
+set test_args=-D _DEBUG -D TESTING
+ 
+@rem I don't know why, but you have to add this.
+@rem Otherwise this doesn't always return 0 when used in cmd.
+@rem I think it's fine in other terminals.
+exit /b 0 
